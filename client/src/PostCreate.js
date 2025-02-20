7add44c5da80676d1996d4d7a5d7ca34cb7c6688
Sherryz707 <shahrbanobokhari@gmail.com>
2025-02-20 11:18:25 +0500

    created post create comp and added Bootstrap

---
+++ client/src/PostCreate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default()=>{
+    return <div>
+        <form>
+            <div className='form-group'>
+                <lable>Title</lable>
+                <input className='form-control'/>
+            </div>
+            <button className='btn btn-primary'>Submit</button>
+        </form>
+    </div>
+}
