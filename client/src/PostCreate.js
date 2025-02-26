3ee44c42b1ff2806a407daf05fbead289b05c686
Sherryz707 <shahrbanobokhari@gmail.com>
2025-02-26 15:43:33 +0500

    implemented Frontend

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios'
 
 export default()=>{
+    const [title,setTitle]=useState('')
+    const onSubmit=async (e)=>{
+        e.preventDefault();
+
+    await axios.post('http://localhost:4000/posts',{
+        title
+    });
+    setTitle('')
+
+    }
     return <div>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className='form-group'>
                 <lable>Title</lable>
-                <input className='form-control'/>
+                <input value={title} onChange={e=>setTitle(e.target.value)} className='form-control'/>
             </div>
             <button className='btn btn-primary'>Submit</button>
         </form>
